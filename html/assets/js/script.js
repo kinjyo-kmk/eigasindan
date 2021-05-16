2f46d20abeb0ec8266b999b550a8d01658f64482
miki kinjyo <kinjyo@razona.jp>
2021-05-16 14:25:35 +0900

    [add] アクセス時に説明用のポップアップを表示

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,1 +1,15 @@
 console.log('aaa')
+
+// アクセスする度にポップアップを表示
+window.addEventListener('load', function() {
+  if(!localStorage.getItem('disp_popup')) {
+    setTimeout(function() {
+      this.sessionStorage.setItem('disp_popup', 'on');
+      var popup = document.getElementsByClassName('popup-wrap');
+      popup[0].classList.add('js-active');
+      popup[0].onclick = function() {
+        popup[0].classList.remove('js-active');
+      }
+    }, 1000);
+  }
+}, false);
